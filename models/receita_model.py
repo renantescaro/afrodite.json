cd559378533617d72759b6fd743c9571d085b690
renantescaro <renantescaro@yahoo.com.br>
2022-02-27 19:49:58 -0300

    adicionado tipo

--- models/receita_model.py
+++ models/receita_model.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
 from models.banco import base as Base
 
 
 class ReceitaModel(Base):
     __tablename__ = 'receita'
 
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, unique=True, index=False)
+    id:int = Column(Integer, primary_key=True, index=True)
+    nome:str = Column(String, unique=True, index=False)
