8d527450744174ac7f1dc11bf8c9398c81eea8f3
renantescaro <renantescaro@yahoo.com.br>
2022-02-27 18:29:47 -0300

    adicionando imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from models.receita_model import ReceitaModel
+from models.receita_secao_model import ReceitaSecaoModel
 from models.secao_model import SecaoModel
+from models.secao_conteudo_model import SecaoConteudoModel
 from models.conteudo_model import ConteudoModel
