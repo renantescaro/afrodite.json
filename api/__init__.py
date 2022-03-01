7186b8f5fd92e8a698ba4dea18b2bcf38c1f2f04
renantescaro <renantescaro@yahoo.com.br>
2022-02-28 21:22:43 -0300

    movendo consultas para services

--- api/__init__.py
+++ api/__init__.py
@@ -1,24 +1,21 @@
 from typing import List
 from fastapi import FastAPI
-from sqlalchemy import select
-from models.banco import session_local
-from models import ReceitaModel
 from services.receita_sv import ReceitaSv
 from .responses import ReceitaCompleta, Receita
 
-app = FastAPI()
+app = FastAPI(title='Api Receitas')
 
 
 @app.get('/receitas', response_model=List[Receita])
 def todas_receitas():
-    return session_local.execute(select(ReceitaModel)).scalars().all()
+    return ReceitaSv().lista_todas_receitas()
 
 
 @app.get('/receitas/{id}', response_model=ReceitaCompleta)
 def receita_por_id(id:int):
-    return ReceitaSv().conteudos_por_receita_id(id)
+    return ReceitaSv().lista_receitas_por_id(id)
 
 
-@app.get('/receitas/{itens}', response_model=List[ReceitaCompleta])
-def receita_por_itens(itens:str):
-    return {'item_id': itens}
+@app.get('/receitas/item/{item}', response_model=List[Receita])
+def receitas_por_item(item:str):
+    return ReceitaSv().lista_receitas_por_conteudo(item)
