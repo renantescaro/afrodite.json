bca7dd17f248aca3c92d58e52128cf4aa335fb01
renantescaro <renantescaro@yahoo.com.br>
2022-03-16 22:34:21 -0300

    adicionando limite de 100 registros

--- services/conteudo_sv.py
+++ services/conteudo_sv.py
@@ -5,15 +5,17 @@ from models import (ConteudoModel)
 
 class IngredienteSv:
     def lista_todos_ingredientes(self):
-        query = select(ConteudoModel)
+        query = select(ConteudoModel).limit(100)
         return session_local.execute(query).scalars().all()
 
 
     def lista_ingredientes_por_nome(self, ingrediente:str):
         query = select(
             ConteudoModel
-        ).where(
+        ).\
+        where(
             ConteudoModel.item.like('%{}%'.format(ingrediente))
-        )
+        ).\
+        limit(100)
 
         return session_local.execute(query).scalars().all()
