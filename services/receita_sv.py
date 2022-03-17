31aa55309ead47d2618e174dd001a51ed6370a76
renantescaro <renantescaro@yahoo.com.br>
2022-03-16 22:29:09 -0300

    correção método receita_por_id e criação de método lista_receitas_por_nome

--- services/receita_sv.py
+++ services/receita_sv.py
@@ -9,24 +9,25 @@ class ReceitaSv:
         return session_local.execute(query).scalars().all()
 
 
-    def lista_receitas_por_id(self, receita_id:int):
-        receita = session_local.execute(select(ReceitaModel).where(ReceitaModel.id == receita_id)).one()
-        receita_nome = dict(receita)['ReceitaModel'].nome
+    def receita_por_id(self, receita_id:int):
+        receita_bd = session_local.execute(select(ReceitaModel).where(ReceitaModel.id == receita_id)).one()
+        receita = dict(receita_bd)['ReceitaModel']
 
         receita_secoes = session_local.execute(
             select(ReceitaSecaoModel).
             where(ReceitaSecaoModel.id_receita == receita_id)
         ).scalars().all()
 
-        receita_json = {'receita': receita_nome, 'itens': []}
+        receita_json = {'receita': receita.nome, 'itens': []}
         for receita_secao in receita_secoes:
-            secao = session_local.execute(select(SecaoModel).where(SecaoModel.id == receita_secao.id)).one()
-            receita_model = dict(secao)['SecaoModel']
+            secao = session_local.execute(select(SecaoModel).where(SecaoModel.id == receita_secao.id_secao)).one()
+            secao_model = dict(secao)['SecaoModel']
 
-            secao_conteudos = session_local.execute(
-                select(SecaoConteudoModel).
-                where(SecaoConteudoModel.id_secao == receita_model.id)
-            ).scalars().all()
+            query = select(SecaoConteudoModel).where(
+                SecaoConteudoModel.id_secao == secao_model.id,
+                SecaoConteudoModel.id_receita == receita.id
+            )
+            secao_conteudos = session_local.execute(query).scalars().all()
 
             conteudos = []
             for secao_conteudo in secao_conteudos:
@@ -34,23 +35,39 @@ class ReceitaSv:
                 conteudos.append(dict(conteudo)['ConteudoModel'].item)
 
             receita_json['itens'].append({
-                'secao' : receita_model.nome,
+                'secao' : secao_model.nome,
                 'conteudo' : conteudos
             })
 
         return receita_json
 
 
-    def lista_receitas_por_conteudo(self, conteudo:str):
-        query = select(
-            ReceitaModel.id, ReceitaModel.nome, ConteudoModel.item
-        ).join(SecaoConteudoModel).\
-        join(SecaoModel).\
-        join(ReceitaSecaoModel).\
-        join(ReceitaModel).\
-        where(
-            ConteudoModel.item.like('%{}%'.format(conteudo)),
-            SecaoModel.nome == ' ingredientes'
-        ).group_by(ReceitaModel.id)
+    def lista_receitas_por_conteudo(self, item:str):
+        query = session_local.query(
+            ReceitaModel.id,
+            ReceitaModel.nome,
+            ConteudoModel.item
+        ).\
+        select_from(SecaoConteudoModel).\
+        join(ConteudoModel).\
+        filter(
+            SecaoConteudoModel.id_secao == 1,
+            SecaoConteudoModel.id_receita == ReceitaModel.id,
+            ConteudoModel.item.like('%{}%'.format(item))
+        ).\
+        group_by(ReceitaModel.id).\
+        limit(100)
 
         return session_local.execute(query).mappings().all()
+
+
+    def lista_receitas_por_nome(self, nome:str):
+        query = select(
+            ReceitaModel
+        ).\
+        where(
+            ReceitaModel.nome.like('%{}%'.format(nome))
+        ).\
+        limit(100)
+
+        return session_local.execute(query).scalars().all()
