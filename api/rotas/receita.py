39bf6ba47fcaee614c4d8fbb14db5bc7f9924483
renantescaro <renantescaro@yahoo.com.br>
2022-03-17 13:30:18 -0300

    criando rotas busca por receita

--- api/rotas/receita.py
+++ api/rotas/receita.py
@@ -1,6 +1,6 @@
 from api import app
 from services.receita_sv import ReceitaSv
-from api.responses import ReceitaCompleta, Receita
+from api.responses import ItemComReceita, ReceitaCompleta, Receita
 from fastapi_pagination import Page, paginate, add_pagination
 
 
@@ -11,11 +11,16 @@ def todas_receitas():
 
 @app.get('/receitas/{id}', response_model=ReceitaCompleta, tags=['Receita'])
 def receita_por_id(id:int):
-    return ReceitaSv().lista_receitas_por_id(id)
+    return ReceitaSv().receita_por_id(id)
 
 
-@app.get('/receitas/ingrediente/{ingrediente}', response_model=Page[Receita], tags=['Receita'])
-def receitas_por_ingrediente(item:str):
+@app.get('/receitas/busca/{nome}', response_model=Page[Receita], tags=['Receita'])
+def busca_receitas_por_nome(nome:str):
+    return paginate(ReceitaSv().lista_receitas_por_nome(nome))
+
+
+@app.get('/receitas/ingrediente/busca/{item}', response_model=Page[ItemComReceita], tags=['Receita'])
+def busca_receitas_por_ingrediente(item:str):
     return paginate(ReceitaSv().lista_receitas_por_conteudo(item))
 
 
