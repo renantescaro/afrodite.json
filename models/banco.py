031dfdd10e38a3945a67a722b049bae0bac7f90f
renantescaro <renantescaro@yahoo.com.br>
2022-03-17 22:19:08 -0300

    carregando url de conexão do banco do .env

--- models/banco.py
+++ models/banco.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = 'mysql://root:SENHA_BANCO@localhost/afrodite_api'
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
@@ -12,4 +16,3 @@ session_local = SessionLocal()
 
 base = declarative_base()
 
-base.metadata.create_all(engine)
