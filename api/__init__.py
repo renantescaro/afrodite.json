fec613d5d8584b19f48984802e1c9c5ac908ee83
renantescaro <renantescaro@yahoo.com.br>
2022-03-17 22:17:08 -0300

    configuração de cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='Api Receitas')
+
+origins = [
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 from api.rotas import *
